8566c53b5facba390b27a5fde02179a5591093b7
ravel01106 <amandaravelocabrera@gmail.com>
2024-07-24 16:37:09 +0100

    feat(App.tsx): created welcome page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'buttonColor': "#00E1C6",
+        'buttonHover': "#21A091",
+        'titleColor' : "#150099",
+      },
+      spacing: {
+        'mainContainerWidth': '60vw',
+        'mainContainerHeigth': '80vh',
+        'buttonsContainerWidth': "40vw",
+        'subContainerWidth': "32rem",
+      },
+      borderRadius: {
+        'none': '0',
+        'medium': '40px',
+      },
+    },
   },
   plugins: [],
 }
