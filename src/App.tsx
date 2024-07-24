25eaf5ddc8d249cb54dc9a56f527d13272c934f1
ravel01106 <amandaravelocabrera@gmail.com>
2024-07-24 12:52:44 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h2 className="text-3xl font-bold underline">
+      Hello world!
+    </h2>
+    </div>
+  )
+}
+
+export default App
