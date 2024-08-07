68c5a29c27a57ca4d6b5dff28a958354ef160c5f
ravel01106 <amandaravelocabrera@gmail.com>
2024-08-07 13:19:25 +0100

    refactor: moved WelcomeScreen  to infrastructure folder

--- src/infrastructure/UI/pages/WelcomePage/WelcomeScreen.tsx
+++ src/infrastructure/UI/pages/WelcomePage/WelcomeScreen.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom"
-import Title from "../components/Title"
+import Title from "../../../../components/Title";
 
 const WelcomeScreen = () => {
   const linkToValidatorFrom = "/validator";
