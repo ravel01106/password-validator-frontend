bde92de6df83e7e5fceccea1186321f68e94c85d
ravel01106 <amandaravelocabrera@gmail.com>
2024-08-09 12:56:07 +0100

    feat: added the necessary conditions to show which parts have been fulfilled when validating the password

--- src/infrastructure/UI/pages/PasswordValidatorPage/components/MessageResultConditions/MessageResultConditions.tsx
+++ src/infrastructure/UI/pages/PasswordValidatorPage/components/MessageResultConditions/MessageResultConditions.tsx
@@ -1,4 +1,5 @@
 import { FaCheckCircle } from 'react-icons/fa'
+import { FaCircleXmark } from "react-icons/fa6";
 import { MessageResultConditionsProps } from './MessageResultConditionsProps'
 
 const MessageResultConditions = ({passwordValidatorResult}: MessageResultConditionsProps) => {
@@ -8,11 +9,11 @@ const MessageResultConditions = ({passwordValidatorResult}: MessageResultConditi
       flex flex-col justify-evenly items-center`}>
         <p className="text-2xl text-black font-bold">The password is {passwordValidatorResult.valid ? "valid": "not valid"} !!!</p>
         <div className="w-96">
-          <nav className="flex flex-row text-black text-xl pb-1"> <FaCheckCircle /> <p className="px-4">Check length</p></nav>
-          <nav className="flex flex-row text-black text-xl pb-1"> <FaCheckCircle /> <p className="px-4">Check uppercase</p></nav>
-          <nav className="flex flex-row text-black text-xl pb-1"> <FaCheckCircle /> <p className="px-4">Check numbers</p></nav>
-          <nav className="flex flex-row text-black text-xl pb-1"> <FaCheckCircle /> <p className="px-4">Check underscore</p></nav>
-          <nav className="flex flex-row text-black text-xl pb-1"> <FaCheckCircle /> <p className="px-4">Check lowercase</p></nav>
+          <nav className="flex flex-row text-black text-xl pb-1"> {passwordValidatorResult.lengthValid ? (<FaCheckCircle />): (<FaCircleXmark />)} <p className="px-4">Check length</p></nav>
+          <nav className="flex flex-row text-black text-xl pb-1"> {passwordValidatorResult.containUpperCase ? (<FaCheckCircle />): (<FaCircleXmark />)} <p className="px-4">Check uppercase</p></nav>
+          <nav className="flex flex-row text-black text-xl pb-1"> {passwordValidatorResult.containLowerCase ? (<FaCheckCircle />): (<FaCircleXmark />)} <p className="px-4">Check numbers</p></nav>
+          <nav className="flex flex-row text-black text-xl pb-1"> {passwordValidatorResult.containNumber ? (<FaCheckCircle />): (<FaCircleXmark />)} <p className="px-4">Check underscore</p></nav>
+          <nav className="flex flex-row text-black text-xl pb-1"> {passwordValidatorResult.containUnderscore ? (<FaCheckCircle />): (<FaCircleXmark />)} <p className="px-4">Check lowercase</p></nav>
         </div>
       </div>
   )
